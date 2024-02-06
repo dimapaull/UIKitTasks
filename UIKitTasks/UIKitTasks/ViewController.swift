ecb340db10b97ffc1a65fe0aeef242b0f28cb77f
Dmitry Pavlov <dimapaull@yandex.ru>
2024-02-06 14:40:31 +0300

    add swift lint

--- UIKitTasks/UIKitTasks/ViewController.swift
+++ UIKitTasks/UIKitTasks/ViewController.swift
@@ -6,14 +6,12 @@
 //
 
 import UIKit
-
+/// Class View Controller
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
 
-
 }
-
