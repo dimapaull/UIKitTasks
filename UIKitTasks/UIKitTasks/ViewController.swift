7253b9eb937c61d64a0dc7846142c76c810865b2
Dmitry Pavlov <dimapaull@yandex.ru>
2024-02-06 14:34:15 +0300

    init

---
+++ UIKitTasks/UIKitTasks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitTasks
+//
+//  Created by Dmitry Pavlov on 6.02.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
