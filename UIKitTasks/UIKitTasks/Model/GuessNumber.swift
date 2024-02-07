936735f04e90d6c21302614235f8289f78c0b145
Dmitriy Pavlov <dimapaull@yandex.ru>
2024-02-08 02:51:39 +0300

    add additional feauters in game

--- UIKitTasks/UIKitTasks/Model/GuessNumber.swift
+++ UIKitTasks/UIKitTasks/Model/GuessNumber.swift
@@ -3,11 +3,29 @@
 
 /// Guess Number struct
 struct GuessNumber {
-    var conceivedNumber: Int {
-        Int.random(in: 1 ... 10)
+    enum ResultState {
+        case right
+        case fail
+        case less
+        case more
     }
 
-    func checkNumber(_ number: Int) -> Bool {
-        number == conceivedNumber ? true : false
+    let conceivedNumber = Int.random(in: 1 ... 10)
+
+    func checkNumber(_ number: Int) -> ResultState {
+        var state = ResultState.fail
+
+        switch number {
+        case conceivedNumber:
+            state = .right
+        case _ where number < conceivedNumber:
+            state = .less
+        case _ where number > conceivedNumber:
+            state = .more
+        default:
+            state = .fail
+        }
+
+        return state
     }
 }
