b1c067db8002d194875c1d0fd9ae88c39250f472
Dmitriy Pavlov <dimapaull@yandex.ru>
2024-02-11 21:31:23 +0300

    add user model and finish ui

--- UIKitTasks/UIKitTasks/SceneDelegate.swift
+++ UIKitTasks/UIKitTasks/SceneDelegate.swift
@@ -11,6 +11,11 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         willConnectTo session: UISceneSession,
         options connectionOptions: UIScene.ConnectionOptions
     ) {
-        // guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.windowScene = windowScene
+        window?.makeKeyAndVisible()
+        let navigationController = UINavigationController(rootViewController: LoginViewController())
+        window?.rootViewController = navigationController
     }
 }
