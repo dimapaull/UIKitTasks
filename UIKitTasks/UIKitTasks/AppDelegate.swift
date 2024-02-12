a5b0d1de9b6d2e7020d8c6fa65ed48ade2a3e06f
VsIakushev <vsiakushev@gmail.com>
2024-02-12 21:24:08 +0300

    Настроил навигейшн, начал верстку ProductConfigurateViewController

--- UIKitTasks/UIKitTasks/AppDelegate.swift
+++ UIKitTasks/UIKitTasks/AppDelegate.swift
@@ -5,22 +5,59 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
     func application(
         _ application: UIApplication,
-
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
-        true
-    }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(
-        _ application: UIApplication,
-
-        configurationForConnecting connectingSceneSession: UISceneSession,
-        options: UIScene.ConnectionOptions
-    ) -> UISceneConfiguration {
-        UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+        let loginViewController = ProductConfigurateViewController()
+        let navigationController = UINavigationController(rootViewController: loginViewController)
+        navigationController.navigationBar.isHidden = true // Скрыть навигационную панель, если не требуется
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+        return true
     }
 }
+
+// @main
+// class AppDelegate: UIResponder, UIApplicationDelegate {
+//    func application(
+//        _ application: UIApplication,
+//
+//        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+//    ) -> Bool {
+//        true
+//    }
+//
+//    // MARK: UISceneSession Lifecycle
+//
+//    func application(
+//        _ application: UIApplication,
+//
+//        configurationForConnecting connectingSceneSession: UISceneSession,
+//        options: UIScene.ConnectionOptions
+//    ) -> UISceneConfiguration {
+//        UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+//    }
+// }
+
+// @main
+// class AppDelegate: UIResponder, UIApplicationDelegate {
+//    var window: UIWindow?
+//
+//    func application(
+//        _ application: UIApplication,
+//        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+//    ) -> Bool {
+//        let window = UIWindow()
+//
+//        window.rootViewController = LoginViewController()
+//        window.makeKeyAndVisible()
+//
+//        self.window = window
+//
+//        return true
+//    }
+// }
--- UIKitTasks/UIKitTasks/Controllers/ProductConfigurateViewController.swift
+++ UIKitTasks/UIKitTasks/Controllers/ProductConfigurateViewController.swift
@@ -5,8 +5,78 @@ import UIKit
 
 /// Контроллер для модификации выбранного продукта
 final class ProductConfigurateViewController: UIViewController {
+    // MARK: - Private Properties
+
+    private enum Roast {
+        case dark
+        case light
+    }
+
+    private let currentCoffeeImageView = UIImageView()
+    private let coffeeImageBackgroundView = UIView()
+    private let chooseCoffeeSegmentedControl = UISegmentedControl()
+    private let modificationLabel = UILabel()
+
+    private let roastButton = UIButton()
+    private let additionalIngredientsButton = UIButton()
+
+    private let priceLabel = UILabel()
+
+    private let orderButton = UIButton()
+
+    let coffeeTypes = ["Американо", "Капучино", "Латте"]
+
+    // MARK: - Overrides Methods (View Life Cycles)
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        setupCoffeeImageBackground()
+        setupNavigationBar()
+
+        addViews()
+    }
+
+    private func setupNavigationBar() {
+        navigationController?.navigationBar.isHidden = false
+
+        let backButton = UIButton(type: .system)
+        backButton.frame = CGRect(x: 0, y: 0, width: 44, height: 44)
+        backButton.backgroundColor = #colorLiteral(red: 0.920884192, green: 0.9655141234, blue: 0.9699655175, alpha: 1)
+        backButton.layer.cornerRadius = 22
+        backButton.setImage(UIImage(named: "leftArrow"), for: .normal)
+        backButton.tintColor = .black
+        backButton.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
+
+        let shareButton = UIButton(type: .system)
+        shareButton.frame = CGRect(x: 0, y: 0, width: 21, height: 21)
+        shareButton.setImage(UIImage(named: "plane"), for: .normal)
+        shareButton.tintColor = .black
+        shareButton.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
+
+        let backBarButtonItem = UIBarButtonItem(customView: backButton)
+        let shareButtonItem = UIBarButtonItem(customView: shareButton)
+
+        navigationItem.leftBarButtonItem = backBarButtonItem
+        navigationItem.rightBarButtonItem = shareButtonItem
+    }
+
+    private func setupCoffeeImageBackground() {
+        coffeeImageBackgroundView.backgroundColor = #colorLiteral(red: 0.9407027364, green: 0.8816927075, blue: 0.8163741231, alpha: 1)
+        coffeeImageBackgroundView.frame = CGRect(x: 0, y: 0, width: 375, height: 346)
+        coffeeImageBackgroundView.layer.cornerRadius = 12
+        coffeeImageBackgroundView.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
+    }
+
+    private func addViews() {
+        view.addSubview(coffeeImageBackgroundView)
+    }
+
+    @objc private func backButtonTapped() {
+        navigationController?.popViewController(animated: true)
+    }
+
+    @objc private func shareButtonPressed() {
+        print("Share with friend")
     }
 }
