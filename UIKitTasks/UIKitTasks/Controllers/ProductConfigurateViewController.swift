4de1c286506bd771b93d8e786b3f5bd778c605e7
VsIakushev <vsiakushev@gmail.com>
2024-02-13 03:01:59 -0800

    Сделал UI экрана ProductConfigurateViewController
    Закончил работу над UI

--- UIKitTasks/UIKitTasks/Controllers/ProductConfigurateViewController.swift
+++ UIKitTasks/UIKitTasks/Controllers/ProductConfigurateViewController.swift
@@ -5,6 +5,12 @@ import UIKit
 
 /// Контроллер для модификации выбранного продукта
 final class ProductConfigurateViewController: UIViewController {
+    // MARK: - Public Properties
+
+    var basePrice = 0
+    var additionalPrice = 0
+    var totalPrice = 0
+
     // MARK: - Private Properties
 
     private enum Roast {
@@ -12,19 +18,27 @@ final class ProductConfigurateViewController: UIViewController {
         case light
     }
 
+    enum AdditionalIngredientsStatus {
+        case add
+        case added
+    }
+
     private let currentCoffeeImageView = UIImageView()
     private let coffeeImageBackgroundView = UIView()
-    private let chooseCoffeeSegmentedControl = UISegmentedControl()
+
+    var selectedCoffeeName = ""
+    private var chooseCoffeeSegmentedControl = UISegmentedControl()
     private let modificationLabel = UILabel()
 
     private let roastButton = UIButton()
     private let additionalIngredientsButton = UIButton()
 
-    private let priceLabel = UILabel()
+    private let totalPriceLabel = UILabel()
 
     private let orderButton = UIButton()
 
     let coffeeTypes = ["Американо", "Капучино", "Латте"]
+    let coffeePrices = [100, 120, 150]
     let coffeeImages = [
         UIImage(named: "coffeeAmericano"),
         UIImage(named: "coffeeCapuchino"),
@@ -40,16 +54,17 @@ final class ProductConfigurateViewController: UIViewController {
 
         setupCoffeeImageBackground()
         setupCurrentCoffeeImageView()
+        setupSegmentedControl()
+        setupModificationLabel()
         setupRoastButton(roast: .light)
+        setupAdditionalIngredientsButton(status: .added)
+        setupTotalPriceLabel()
+        setupOrderButton()
 
         addViews()
     }
 
-    private func setupCurrentCoffeeImageView() {
-        currentCoffeeImageView.frame = CGRect(x: 112, y: 128, width: 150, height: 150)
-        currentCoffeeImageView.image = coffeeImages[0]
-    }
-
+    /// Настройка панели навигации
     private func setupNavigationBar() {
         navigationController?.navigationBar.isHidden = false
 
@@ -65,7 +80,7 @@ final class ProductConfigurateViewController: UIViewController {
         shareButton.frame = CGRect(x: 0, y: 0, width: 21, height: 21)
         shareButton.setImage(UIImage(named: "plane"), for: .normal)
         shareButton.tintColor = .black
-        shareButton.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
+        shareButton.addTarget(self, action: #selector(shareButtonTapped), for: .touchUpInside)
 
         let backBarButtonItem = UIBarButtonItem(customView: backButton)
         let shareButtonItem = UIBarButtonItem(customView: shareButton)
@@ -74,6 +89,36 @@ final class ProductConfigurateViewController: UIViewController {
         navigationItem.rightBarButtonItem = shareButtonItem
     }
 
+    /// Настройка фона изображения кофе
+    private func setupCoffeeImageBackground() {
+        coffeeImageBackgroundView.backgroundColor = #colorLiteral(red: 0.9407027364, green: 0.8816927075, blue: 0.8163741231, alpha: 1)
+        coffeeImageBackgroundView.frame = CGRect(x: 0, y: 0, width: 375, height: 346)
+        coffeeImageBackgroundView.layer.cornerRadius = 12
+        coffeeImageBackgroundView.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
+    }
+
+    /// Настройка изображения кофе
+    private func setupCurrentCoffeeImageView() {
+        currentCoffeeImageView.frame = CGRect(x: 112, y: 128, width: 150, height: 150)
+        currentCoffeeImageView.image = coffeeImages[0]
+    }
+
+    /// Настройка Segmented Control
+    private func setupSegmentedControl() {
+        chooseCoffeeSegmentedControl = UISegmentedControl(items: coffeeTypes)
+        chooseCoffeeSegmentedControl.frame = CGRect(x: 15, y: 368, width: 345, height: 44)
+        chooseCoffeeSegmentedControl.addTarget(self, action: #selector(selectedCoffee), for: .valueChanged)
+        chooseCoffeeSegmentedControl.selectedSegmentIndex = 0
+    }
+
+    /// Настройка лейбла "Модификация"
+    private func setupModificationLabel() {
+        modificationLabel.frame = CGRect(x: 15, y: 432, width: 200, height: 30)
+        modificationLabel.text = "Модификация"
+        modificationLabel.font = UIFont(name: "Verdana-Bold", size: 18)
+    }
+
+    /// Настройка кнопки выбора обжарки
     private func setupRoastButton(roast: Roast) {
         roastButton.frame = CGRect(x: 15, y: 482, width: 165, height: 165)
         roastButton.layer.cornerRadius = 12
@@ -107,30 +152,115 @@ final class ProductConfigurateViewController: UIViewController {
         roastButton.addSubview(imageView)
     }
 
-    private func setupCoffeeImageBackground() {
-        coffeeImageBackgroundView.backgroundColor = #colorLiteral(red: 0.9407027364, green: 0.8816927075, blue: 0.8163741231, alpha: 1)
-        coffeeImageBackgroundView.frame = CGRect(x: 0, y: 0, width: 375, height: 346)
-        coffeeImageBackgroundView.layer.cornerRadius = 12
-        coffeeImageBackgroundView.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
+    /// Настройка кнопки выбора дополнительных ингредиентов
+    private func setupAdditionalIngredientsButton(status: AdditionalIngredientsStatus) {
+        additionalIngredientsButton.frame = CGRect(x: 195, y: 482, width: 165, height: 165)
+        additionalIngredientsButton.layer.cornerRadius = 12
+        additionalIngredientsButton.backgroundColor = #colorLiteral(red: 0.9694761634, green: 0.9694761634, blue: 0.9694761634, alpha: 1)
+
+        var image: UIImage?
+        var imageView = UIImageView()
+
+        switch status {
+        case .add:
+            image = UIImage(named: "addCross")
+            imageView = UIImageView(frame: CGRect(x: 67, y: 56, width: 30, height: 30))
+        case .added:
+            image = UIImage(named: "customCheckmark")
+            imageView = UIImageView(frame: CGRect(x: 57, y: 46, width: 50, height: 50))
+        }
+
+        additionalIngredientsButton.setTitle("Дополнительные ингредiенты", for: .normal)
+
+        imageView.image = image
+        imageView.contentMode = .scaleAspectFit
+
+        additionalIngredientsButton.setTitleColor(.black, for: .normal)
+        additionalIngredientsButton.titleLabel?.font = UIFont(name: "Verdana", size: 14)
+        additionalIngredientsButton.titleLabel?.numberOfLines = 2
+        additionalIngredientsButton.titleLabel?.textAlignment = .center
+
+        // Размещение надписи под кнопкой
+        additionalIngredientsButton.titleEdgeInsets = UIEdgeInsets(top: 100, left: 0, bottom: 0, right: 0)
+
+        additionalIngredientsButton.addTarget(
+            self,
+            action: #selector(additionalIngredientsButtonTapped),
+            for: .touchUpInside
+        )
+
+        additionalIngredientsButton.addSubview(imageView)
+    }
+
+    /// Настройка лейбла цены
+    private func setupTotalPriceLabel() {
+        totalPriceLabel.text = "Цѣна \(coffeePrices[0])"
+        totalPriceLabel.frame = CGRect(x: 15, y: 669, width: 345, height: 30)
+        totalPriceLabel.font = UIFont(name: "Verdana-Bold", size: 18)
+        totalPriceLabel.textAlignment = .right
+    }
+
+    private func setupOrderButton() {
+        orderButton.setTitle("Заказать", for: .normal)
+        orderButton.setTitleColor(.white, for: .normal)
+        orderButton.titleLabel?.font = UIFont(name: "Verdana-Bold", size: 18)
+        orderButton.frame = CGRect(x: 15, y: 717, width: 345, height: 53)
+        orderButton.layer.cornerRadius = 12
+        orderButton.backgroundColor = #colorLiteral(red: 0.3492000699, green: 0.7459038496, blue: 0.7814769149, alpha: 1)
+        orderButton.addTarget(self, action: #selector(orderButtonTapped), for: .touchUpInside)
     }
 
     private func addViews() {
         view.addSubview(coffeeImageBackgroundView)
         coffeeImageBackgroundView.addSubview(currentCoffeeImageView)
-        view.addSubview(roastButton)
+
         view.addSubview(currentCoffeeImageView)
+        view.addSubview(chooseCoffeeSegmentedControl)
+        view.addSubview(modificationLabel)
+        view.addSubview(roastButton)
+        view.addSubview(additionalIngredientsButton)
+        view.addSubview(totalPriceLabel)
+        view.addSubview(orderButton)
+    }
+
+    private func updateTotalPrice() {
+        totalPrice = basePrice + additionalPrice
+        totalPriceLabel.text = "Цѣна \(totalPrice)"
+    }
+
+    // обработка изменений выбора кофе
+    @objc func selectedCoffee(target: UISegmentedControl) {
+        if target.isEqual(chooseCoffeeSegmentedControl) {
+            let segmentIndex = target.selectedSegmentIndex
+            currentCoffeeImageView.image = coffeeImages[segmentIndex]
+            basePrice = coffeePrices[segmentIndex]
+            selectedCoffeeName = target.titleForSegment(at: segmentIndex) ?? ""
+            updateTotalPrice()
+            print(selectedCoffeeName)
+            print(basePrice)
+        }
     }
 
     @objc private func roastButtonTapped() {
         // Обработка нажатия на кнопку
-        print("Button tapped!")
+        print("Roast Button tapped!")
+    }
+
+    @objc func additionalIngredientsButtonTapped() {
+        // Обработка нажатия на кнопку
+        print("Ingredients Button tapped!")
     }
 
     @objc private func backButtonTapped() {
+        print("Back Button tapped!")
         navigationController?.popViewController(animated: true)
     }
 
-    @objc private func shareButtonPressed() {
+    @objc private func orderButtonTapped() {
+        print("Order Button tapped!")
+    }
+
+    @objc private func shareButtonTapped() {
         print("Share with friend")
     }
 }
