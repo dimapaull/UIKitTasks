b30bb2e4f2fb1caaa9406af7515be6c815a96b22
Dmitriy Pavlov <dimapaull@yandex.ru>
2024-02-15 12:36:46 +0300

    edit scene

--- UIKitTasks/UIKitTasks/AppDelegate.swift
+++ UIKitTasks/UIKitTasks/AppDelegate.swift
@@ -5,18 +5,21 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    var window: UIWindow?
-
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
-        let loginViewController = ProductConfigurateViewController()
-        let navigationController = UINavigationController(rootViewController: loginViewController)
-        navigationController.navigationBar.isHidden = true 
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = navigationController
-        window?.makeKeyAndVisible()
-        return true
+        true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(
+        _ application: UIApplication,
+
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 }
--- UIKitTasks/UIKitTasks/SceneDelegate.swift
+++ UIKitTasks/UIKitTasks/SceneDelegate.swift
@@ -3,4 +3,21 @@
 
 import UIKit
 
-final class SceneDelegate: UIResponder, UIWindowSceneDelegate { }
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.windowScene = windowScene
+        window?.makeKeyAndVisible()
+        // let loginViewController = LoginViewController()
+        // window?.rootViewController = loginViewController
+        let loginViewController = LoginViewController()
+        window?.rootViewController = loginViewController
+    }
+}
