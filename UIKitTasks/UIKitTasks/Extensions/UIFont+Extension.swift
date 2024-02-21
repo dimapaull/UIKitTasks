f37b3b08963ed830ffad9c8b1d9f264920bc02a0
Dmitriy Pavlov <dimapaull@yandex.ru>
2024-02-22 01:09:37 +0300

    review edit

--- UIKitTasks/UIKitTasks/Extensions/UIFont+Extension.swift
+++ UIKitTasks/UIKitTasks/Extensions/UIFont+Extension.swift
@@ -8,14 +8,14 @@ extension UIFont {
     /// Шрифт Verdana
     /// - Parameter ofSize: Размер необходимого шрифта
     /// - Returns: Шрифт Verdana
-    func verdana(ofSize: CGFloat) -> UIFont {
+    static func verdana(ofSize: CGFloat) -> UIFont {
         UIFont(name: "Verdana", size: ofSize) ?? UIFont.systemFont(ofSize: ofSize)
     }
 
     /// Шрифт Verdana Bold
     /// - Parameter ofSize: Размер необходимого шрифта
     /// - Returns: Шрифт Verdana Bold
-    func verdanaBold(ofSize: CGFloat) -> UIFont {
+    static func verdanaBold(ofSize: CGFloat) -> UIFont {
         UIFont(name: "Verdana-Bold", size: ofSize) ?? UIFont.systemFont(ofSize: ofSize)
     }
 }
---
+++ UIKitTasks/UIKitTasks/FeedScreen/RecommendView.swift
@@ -0,0 +1,8 @@
+//
+//  RecommendView.swift
+//  UIKitTasks
+//
+//  Created by Dmitry Pavlov on 21.02.24.
+//
+
+import Foundation
