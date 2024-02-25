68fa074824adc2c4b193787e6c7bbbc7c2ec6dca
Dmitriy Pavlov <dimapaull@yandex.ru>
2024-02-26 00:11:17 +0300

    add anchors vc

---
+++ UIKitTasks/UIKitTasks/AnchorsViewController.swift
@@ -0,0 +1,83 @@
+// AnchorsViewController.swift
+// Copyright © RoadMap. All rights reserved.
+
+import UIKit
+
+/// Экран со светофором на якорях
+final class AnchorsViewController: UIViewController {
+    // MARK: - Visual Components
+
+    private let yellowView = {
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        view.backgroundColor = .yellow
+        return view
+    }()
+
+    private let redView = {
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        view.backgroundColor = .red
+        return view
+    }()
+
+    private let greenView = {
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        view.backgroundColor = .green
+        return view
+    }()
+
+    private let blackView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .black
+        return view
+    }()
+
+    // MARK: - Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    //MARK: - Private Methods
+
+    private func configureUI() {
+        view.backgroundColor = .white
+        addSubviews()
+        setupAnchors()
+    }
+
+    private func addSubviews() {
+        for item in [blackView, yellowView, redView, greenView] {
+            item.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview(item)
+        }
+    }
+
+    private func setupAnchors() {
+        yellowView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        yellowView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        yellowView.heightAnchor.constraint(lessThanOrEqualToConstant: 130).isActive = true
+        let heightConstraint = yellowView.heightAnchor.constraint(
+            equalTo: view.heightAnchor,
+            multiplier: 0.25
+        )
+        heightConstraint.priority = .defaultLow
+        heightConstraint.isActive = true
+        yellowView.widthAnchor.constraint(equalTo: yellowView.heightAnchor).isActive = true
+
+        redView.widthAnchor.constraint(equalTo: yellowView.widthAnchor).isActive = true
+        redView.heightAnchor.constraint(equalTo: yellowView.heightAnchor).isActive = true
+        redView.bottomAnchor.constraint(equalTo: yellowView.topAnchor, constant: -15).isActive = true
+        redView.centerXAnchor.constraint(equalTo: yellowView.centerXAnchor).isActive = true
+
+        greenView.widthAnchor.constraint(equalTo: yellowView.widthAnchor).isActive = true
+        greenView.heightAnchor.constraint(equalTo: yellowView.heightAnchor).isActive = true
+        greenView.topAnchor.constraint(equalTo: yellowView.bottomAnchor, constant: 15).isActive = true
+        greenView.centerXAnchor.constraint(equalTo: yellowView.centerXAnchor).isActive = true
+
+        blackView.widthAnchor.constraint(equalTo: yellowView.widthAnchor, constant: 30).isActive = true
+        blackView.bottomAnchor.constraint(equalTo: greenView.bottomAnchor, constant: 15).isActive = true
+        blackView.topAnchor.constraint(equalTo: redView.topAnchor, constant: -15).isActive = true
+        blackView.centerXAnchor.constraint(equalTo: yellowView.centerXAnchor).isActive = true
+    }
+}
---
+++ UIKitTasks/UIKitTasks/StackViewController.swift
@@ -0,0 +1,78 @@
+// StackViewController.swift
+// Copyright © RoadMap. All rights reserved.
+
+import UIKit
+
+/// Экран светофора на стэк вью
+final class StackViewController: UIViewController {
+    // MARK: - Visual Components
+
+    private let yellowView = {
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        view.backgroundColor = .yellow
+        return view
+    }()
+
+    private let redView = {
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        view.backgroundColor = .red
+        return view
+    }()
+
+    private let greenView = {
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        view.backgroundColor = .green
+        return view
+    }()
+
+    private let blackView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .black
+        return view
+    }()
+
+    private lazy var stackView = {
+        let stackView = UIStackView(arrangedSubviews: [redView, yellowView, greenView])
+        stackView.spacing = 10
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.backgroundColor = .black
+        stackView.frame = CGRect(x: 0, y: 0, width: 160, height: 376)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+
+    // MARK: - Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    //MARK: - Private Methods
+
+    private func configureUI() {
+        view.backgroundColor = .white
+        addSubviews()
+        setupAnchors()
+    }
+
+    private func addSubviews() {
+        view.addSubview(blackView)
+        view.addSubview(stackView)
+    }
+
+    private func setupAnchors() {
+        stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        stackView.widthAnchor.constraint(equalToConstant: 110).isActive = true
+        stackView.heightAnchor.constraint(equalToConstant: 350).isActive = true
+
+        blackView.centerXAnchor.constraint(equalTo: stackView.centerXAnchor).isActive = true
+        blackView.centerYAnchor.constraint(equalTo: stackView.centerYAnchor).isActive = true
+        blackView.widthAnchor.constraint(equalToConstant: 150).isActive = true
+        blackView.bottomAnchor.constraint(equalTo: stackView.bottomAnchor, constant: 10).isActive = true
+        blackView.topAnchor.constraint(equalTo: stackView.topAnchor, constant: -10).isActive = true
+    }
+}
